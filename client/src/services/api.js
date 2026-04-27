0d788eb6200812a5a1c42f135c34585270d76b51
sahildhatwalia <sahildhatwalia10@gmail.com>
2026-04-27 14:19:51 +0530

    update new things

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/api` 
+  : '/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL,
 });
 
 api.interceptors.request.use((config) => {
