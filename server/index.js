9993e015247ed1778d1eb74f5b66597fa18c4075
sahildhatwalia <sahildhatwalia10@gmail.com>
2026-09-12 20:46:09 +0530

    Fix Express production route

--- server/index.js
+++ server/index.js
@@ -280,14 +280,9 @@ app.post('/api/progress', auth, async (req, res) => {
   await progress.save();
   res.send(progress);
 });
-
 // Serve frontend in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
-  });
-}
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
